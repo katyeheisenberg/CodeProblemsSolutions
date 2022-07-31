3756bd9b0a3bbd88514e58548e0d19c111714eea
kate <chimmi10kate@mail.ru>
2022-08-01 01:15:07 +0300

    wrote solution for the 102 task

--- tasks.js
+++ tasks.js
@@ -24,5 +24,8 @@
 // Math.max() - method which will help us to get the maximum value.
 
 const maxDepth = function (root) {
-
+  if (!root) {
+    return 0
+  }
+  return 1 + Math.max(maxDepth(root.left), maxDepth(root.right))
 }

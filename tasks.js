4850138a929ba624d53337c2c566998bf07b6e3c
kate <chimmi10kate@mail.ru>
2022-08-09 21:14:22 +0300

    wrote solution for the 78. leetcode task

--- tasks.js
+++ tasks.js
@@ -364,3 +364,41 @@ const twoSum = (numbers, target) => { // this solution runs in linear time compl
     }
   }
 };
+
+
+// 78. subsets
+// Given an integer array nums of unique elements, return all possible subsets (the power set).
+// The solution set must not contain duplicate subsets. Return the solution in any order.
+
+// Example 1:
+// Input: nums = [1,2,3]
+// Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+
+// Example 2:
+// Input: nums = [0]
+// Output: [[],[0]]
+
+// Constraints:
+//     1 <= nums.length <= 10
+//     -10 <= nums[i] <= 10
+//     All the numbers of nums are unique.
+
+
+
+// solution (binary)
+const subsets = (nums) => {
+  const countSubsets = Math.pow(2, nums.length);
+  let result = [];
+  for (let i = 0; i < countSubsets; i += 1) {
+    const binaryStr = i.toString(2).padStart(nums.length, "0");
+    let subset = [];
+    for (let j = 0; j < binaryStr.length; j += 1) {
+      if (binaryStr[j] === "1") {
+        subset = [...subset, nums[j]];
+      }
+    }
+    result = [...result, subset];
+  }
+  return result;
+};
+

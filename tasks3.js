fab109c7c37b830da48d3fbe2f78c767d12a5c99
kate <chimmi10kate@mail.ru>
2022-08-31 22:13:40 +0300

    wrote solution for the 36. leetcode task

--- tasks3.js
+++ tasks3.js
@@ -42,14 +42,6 @@
 //     board[i].length == 9
 //     board[i][j] is a digit 1-9 or '.'.
 
-const isValidSudoku = (board) => {
-  
-
-
-
-
-}
-
 const board = [
   ["8", "3", ".", ".", "7", ".", ".", ".", "."],
   ["6", ".", ".", "1", "9", "5", ".", ".", "."],
@@ -61,5 +53,28 @@ const board = [
   [".", ".", ".", "4", "1", "9", ".", ".", "5"],
   [".", ".", ".", ".", "8", ".", ".", "7", "9"],
 ];
+
+
+const isValidSudoku = function(board) {
+    const isUsedInRow = new Array(9).fill(0).map(_ => new Array())
+    const isUsedInCol = new Array(9).fill(0).map(_ => new Array())
+    const isUsedInSub = new Array(9).fill(0).map(_ => new Array())
+
+    for (let i = 0; i < 9; i += 1) {
+        for (let h = 0; h < 9; h += 1) {
+            const cell = board[i][h]
+            if (cell === '.') continue
+            const subBoxIndex = Math.floor(i / 3) + Math.floor(h / 3) * 3
+            if (isUsedInRow[i][cell] || isUsedInCol[h][cell] || isUsedInSub[subBoxIndex][cell]) {
+                return false
+            }
+            isUsedInRow[i][cell] = true
+            isUsedInCol[h][cell] = true
+            isUsedInSub[subBoxIndex][cell] = true
+        }
+    }
+
+    return true
+}
 
 console.log(isValidSudoku(board))

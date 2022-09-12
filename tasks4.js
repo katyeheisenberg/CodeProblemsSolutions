50573d126baa47ccc7639b696ced3937ee60eac5
kate <chimmi10kate@mail.ru>
2022-09-12 10:45:58 +0300

    wrote solution for strip comments task

--- tasks4.js
+++ tasks4.js
@@ -24,3 +24,41 @@ const checkTheOrder = (subs, string) => {
 
 console.log(checkTheOrder("abc", "abraaarba")); // expect false
 console.log(checkTheOrder("abc", "abracadabra")); // expect true
+
+
+// Strip Comments
+// Complete the solution so that it strips all text that follows any of a set of comment markers passed in.
+// Any whitespace at the end of the line should also be stripped out.
+
+// Example:
+// Given an input string of:
+// apples, pears # and bananas
+// grapes
+// bananas !apples
+
+// The output expected would be:
+// apples, pears
+// grapes
+// bananas
+
+const stripComments = (input, markers) => {
+  let  arr = input.split('\n')
+  for (let i = 0; i < markers.length; i += 1) {
+    arr = arr.map((el) => {
+      const index = el.indexOf(markers[i])
+      if (index !== -1) {
+        return el.substring(0, index)
+      } else {
+        return el.replace(/\s*$/, '')
+      }
+    })
+  }
+  return arr.join('\n').trim()
+};
+
+console.log(
+  stripComments("apples, pears # and bananas\ngrapes\nbananas !apples", [
+    "#",
+    "!",
+  ])
+); // expect "apples, pears\ngrapes\nbananas"

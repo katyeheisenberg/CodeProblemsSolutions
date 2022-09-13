2c70ea794e734eafb6c3253c0e5bd02ca7059a55
kate <chimmi10kate@mail.ru>
2022-09-14 01:56:43 +0300

    wrote solution for the next smaller number task

--- tasks4.js
+++ tasks4.js
@@ -62,3 +62,37 @@ console.log(
     "!",
   ])
 ); // expect "apples, pears\ngrapes\nbananas"
+
+
+// Next smaller number with the same digits
+// Write a function that takes a positive integer and returns the next smaller positive integer containing the same digits.
+// For example:
+
+// nextSmaller(21) == 12
+// nextSmaller(531) == 513
+// nextSmaller(2071) == 2017
+
+// Return -1 (for Haskell: return Nothing, for Rust: return None), when there is no smaller number that contains the same digits. Also return -1 when the next smaller number with the same digits would require the leading digit to be zero.
+
+// nextSmaller(9) == -1
+// nextSmaller(111) == -1
+// nextSmaller(135) == -1
+// nextSmaller(1027) == -1 // 0721 is out since we don't write numbers with leading zeros
+//     some tests will include very large numbers.
+//     test data only employs positive integers.
+
+
+const minify = (n) => n.toString().split('').sort().join('').replace(/^(0+)([1-9])/, '$2$1')
+
+function nextSmaller(n) {
+  const min = minify(n);
+  for (let i = n - 1; i >= min; i -= 1) {
+    if (minify(i) === min) {
+      return i
+    }
+  }
+
+  return -1
+}
+
+console.log(nextSmaller(21))

58b2db7822d0bc4d66a8a7eca1dff953125e559f
kate <chimmi10kate@mail.ru>
2022-09-14 11:04:49 +0300

    wrote solution for the Title Case task

--- tasks4.js
+++ tasks4.js
@@ -96,3 +96,34 @@ function nextSmaller(n) {
 }
 
 console.log(nextSmaller(21))
+
+// Ttitle Case
+// A string is considered to be in title case if each word in the string is either (a) capitalised (that is, only the first letter of the word is in upper case)
+// or (b) considered to be an exception and put entirely into lower case unless it is the first word, which is always capitalised.
+// Write a function that will convert a string into title case, given an optional list of exceptions (minor words).
+// The list of minor words will be given as a string with each word separated by a space.
+// Your function should ignore the case of the minor words string -- it should behave in the same way even if the case of the minor word string is changed.
+//     First argument: space-delimited list of minor words that must always be lowercase except for the first word in the string.
+//     Second argument: the original string to be converted.
+
+const capitalize = ([firstChar, ...rest]) =>
+  firstChar.toUpperCase() + rest.join("");
+
+function titleCase(title, minorWords) {
+  if (!title) {
+    return "";
+  }
+  const minorWordArr = minorWords ? minorWords.toLowerCase().split(" ") : [];
+  return title
+    .toLowerCase()
+    .split(" ")
+    .map((item, index) => {
+      if (minorWordArr.indexOf(item) === -1 || index === 0) {
+        return capitalize(item);
+      }
+      return item;
+    })
+    .join(" ");
+}
+
+console.log(titleCase("THE WIND IN THE WILLOWS", "The In")); // expect 'The Wind in the Willows'

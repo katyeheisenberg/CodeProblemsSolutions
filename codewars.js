99accc9619b5f81a6d822889587909c905d12e24
kate <chimmi10kate@mail.ru>
2022-10-06 00:43:32 +0300

    Arrays Similar every method solution

--- codewars.js
+++ codewars.js
@@ -41,3 +41,26 @@ const maxDepthBrackets = (s) => {
 console.log(maxDepthBrackets("((AAX(AB)(UP)F(Z))(HH))")); // expect ['AB', 'UP', 'Z']
 console.log(maxDepthBrackets("AAA")); // expect ["AAA"]
 console.log(maxDepthBrackets("")); // expect [""]
+
+
+// Arrays Similar 6kyu
+// Write a function that determines whether the passed in arrays are similar. Similar means they contain the same elements,
+//  and the same number of occurrences of elements.
+// const arr1 = [1, 2, 2, 3, 4],
+//       arr2 = [2, 1, 2, 4, 3],
+//       arr3 = [1, 2, 3, 4],
+//       arr4 = [1, 2, 3, "4"]
+// arraysSimilar(arr1, arr2); // Should equal true
+// arraysSimilar(arr2, arr3); // Should equal false
+// arraysSimilar(arr3, arr4); // Should equal false
+
+
+const arraysSimilar = (arr1, arr2) => {
+  if(arr1.length !== arr2.length) return false
+  let arr1Sorted = [ ...arr1].sort()
+  let arr2Sorted = [ ...arr2].sort()
+  return arr1Sorted.every((item, index) => item === arr2Sorted[index])
+}
+
+console.log(arraysSimilar([1, 2, 2, 3, 4], [1, 2, 3, 4])); // expect false
+console.log(arraysSimilar([1, 2, 3, "4"], [1, 2, "4", 3])); // expect true

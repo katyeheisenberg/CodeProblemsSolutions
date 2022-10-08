353248b5b009b91665c5e61b4cc4526288ba1fe7
kate <chimmi10kate@mail.ru>
2022-10-09 00:01:12 +0300

    lasDigit problem solution codewars

--- codewars.js
+++ codewars.js
@@ -64,3 +64,23 @@ const arraysSimilar = (arr1, arr2) => {
 
 console.log(arraysSimilar([1, 2, 2, 3, 4], [1, 2, 3, 4])); // expect false
 console.log(arraysSimilar([1, 2, 3, "4"], [1, 2, "4", 3])); // expect true
+
+// Last digit of a large number
+// Define a function that takes in two non-negative integers aaa and bbb and returns the last decimal digit of aba^bab.
+//  Note that aaa and bbb may be very large!
+// For example, the last decimal digit of 979^797 is 999, since 97=47829699^7 = 478296997=4782969.
+//  The last decimal digit of (2200)2300({2^{200}})^{2^{300}}(2200)2300, which has over 109210^{92}1092 decimal digits,
+//  is 666. Also, please take 000^000 to be 111.
+// You may assume that the input will always be valid.
+// Examples
+// lastDigit("4", "1")            // returns 4
+// lastDigit("4", "2")            // returns 6
+// lastDigit("9", "7")            // returns 9
+// lastDigit("10","10000000000")  // returns 0
+
+const lastDigit = (str1, str2) => {
+  return +!+str2 || Math.pow(str1.slice(-1), str2.slice(-2) % 4 || 4) % 10
+}
+console.log(lastDigit("9", "7")); // expect 9
+console.log(lastDigit("4", "2")); // expect 6
+console.log(lastDigit("10", "10000000000")); // expect 0

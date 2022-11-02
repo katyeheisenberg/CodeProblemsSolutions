62dd552b575756688260341f2398c87934814b08
cate <chimmi10kate@mail.ru>
2022-11-03 00:05:41 +0300

    solution for 238 problem

--- tasks6.js
+++ tasks6.js
@@ -22,8 +22,24 @@
 
 // Follow up: Can you solve the problem in O(1) extra space complexity? (The output array does not count as extra space for space complexity analysis.)
 
-const productArray = (arr) => {
+const productArr = (nums) => {
+    let result = new Array(nums.length).fill(nums[0])
+    let variable = nums[nums.length - 1]
 
+    for (let i = 1; i < nums.length; i += 1) {
+        result[i] = result[i - 1] * nums[i]
+    }
+
+    result[nums.length - 1] = result[result.length - 2]
+
+    for (let j = nums.length - 2; j > 0; j -= 1) {
+        result[j] = result[j - 1] * variable
+        variable = variable * nums[j]
+    }
+
+    result[0] = variable
+    return result
 }
 
-console.log(productArr([1,2,4,5,1]))
+console.log(productArr([1,2,4,5,1])) // expect [ 40, 20, 10, 8, 40 ]
+console.log(productArr([1, 2, 3, 4])); // expect [ 24, 12, 8, 6 ]

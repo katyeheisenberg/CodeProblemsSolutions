7bc52077c0383839b4eaad8324b10d6163fa5e9f
cate <chimmi10kate@mail.ru>
2022-11-06 09:26:09 +0300

    wrote solution for 337 task

--- tasks6.js
+++ tasks6.js
@@ -125,3 +125,35 @@ const timeStockFunc = (arr) => {
 
 console.log(timeStockFunc([7, 6, 4, 3, 1])); // expect 0
 console.log(timeStockFunc([7, 1, 5, 3, 6, 4])); // expect 5
+
+
+// 337. House Robber III
+
+// The thief has found himself a new place for his thievery again. There is only one entrance to this area, called root.
+
+// Besides the root, each house has one and only one parent house. After a tour, the smart thief realized that all houses
+// in this place form a binary tree. It will automatically contact the police if two directly-linked houses were broken into on the same night.
+
+// Given the root of the binary tree, return the maximum amount of money the thief can rob without alerting the police.
+
+// Example:
+// Input: root = [3,4,5,1,3,null,1]
+// Output: 9
+// Explanation: Maximum amount of money the thief can rob = 4 + 5 = 9.
+
+const houseR3 = (root) => {
+  function decide(node) {
+    if(!node) return [0,0]
+
+    let [leftRob, leftNot] = decide(node.left)
+    let [rightRob, rightNot] = decide(node.right)
+
+    let notRob = Math.max(leftRob+rightRob, leftRob+rightNot, leftNot+rightNot, leftNot+rightRob)
+    let robDecision = node.val + leftNot + rightNot
+
+    return [robDecision, notRob]
+  }
+
+  return Math.max(...decide(root))
+
+}

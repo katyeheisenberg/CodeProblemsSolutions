7353a0652e668a793a751a4aa9709a786865db54
cate <chimmi10kate@mail.ru>
2022-11-09 01:15:15 +0300

    solution 202 task

--- tasks6.js
+++ tasks6.js
@@ -195,5 +195,51 @@ const rotate = (nums, k) => {
   movePosition(nums, k, nums.length - 1);
 };
 
-console.log([1, 2, 3, 4, 5, 6, 7], 3); // [5,6,7,1,2,3,4]
-console.log([-1, -100, 3, 99], 2); // [3,99,-1,-100]
+
+// 202. Happy Number
+// Write an algorithm to determine if a number n is happy.
+
+// A happy number is a number defined by the following process:
+
+//     Starting with any positive integer, replace the number by the sum of the squares of its digits.
+//     Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+//     Those numbers for which this process ends in 1 are happy.
+
+// Return true if n is a happy number, and false if not.
+
+// Example 1:
+
+// Input: n = 19
+// Output: true
+// Explanation:
+// 12 + 92 = 82
+// 82 + 22 = 68
+// 62 + 82 = 100
+// 12 + 02 + 02 = 1
+
+
+const sumNum = (num) => {
+  let total = 0;
+  while (num !== 0) {
+    let lastNmb = num % 10;
+    num = Math.floor(num / 10);
+    total += Math.pow(lastNmb, 2);
+  }
+
+  return total;
+};
+
+const isHappy = (n) => {
+  let fast = n;
+  let slow = n;
+  while (true) {
+    fast = sumNum(sumNum(fast));
+    slow = sumNum(slow);
+    if (fast === slow) {
+      return fast === 1;
+    }
+  }
+};
+
+console.log(isHappy(19)) // true
+console.log(isHappy(2)) // false

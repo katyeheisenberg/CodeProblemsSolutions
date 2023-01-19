71592bf90dce109062239cd5b4e3fd78cf8a94ec
cate <chimmi10kate@mail.ru>
2023-01-19 17:44:19 +0300

    wrote solution for 529 minesweeper problem

--- minesweeper.ts
+++ minesweeper.ts
@@ -16,3 +16,42 @@
 // Output: [["B","1","E","1","B"],["B","1","M","1","B"],["B","1","1","1","B"],["B","B","B","B","B"]]
 
 
+const updateBoard = (board: string[][], click: number[]): string[][] => {
+  const height = board.length;
+  const width = board[0].length;
+  const x: number = click[1];
+  const y: number = click[0];
+  const dfs = (x: number, y: number) => {
+    if (board[y][x] === 'M') {
+      board[y][x] = 'X';
+      return
+    }
+    const checked: [number, number][] = [];
+
+    for (let curY = y - 1; curY <= y + 1; curY += 1) {
+      for (let curX = x - 1; curX <= x + 1; curX += 1) {
+        if (curX < width && curX >= 0 && curY < height && curY >= 0) {
+          checked.push([curX, curY])
+        }
+      }
+    };
+
+    const minesAmount: number = checked.reduce((acc, [curX, curY]) => acc += board[curY][curX] === 'M' ? 1 : 0, 0);
+
+    if (minesAmount > 0) {
+      board[y][x] = minesAmount.toString();
+    } else {
+      board[y][x] = 'B';
+      for (const [curX, curY] of checked) {
+        if (board[curY][curX] === 'E') {
+          dfs(curX, curY);
+        }
+      }
+    };
+
+  };
+  dfs(x, y);
+  return board;
+};
+
+updateBoard([["E","E","E","E","E"],["E","E","M","E","E"],["E","E","E","E","E"],["E","E","E","E","E"]], click = [3,0]);

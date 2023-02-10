8b1cc167ad4935c6f3ccca0ecad9df8d58936ff5
katyeheisenberg <chimmi10kate@mail.ru>
2023-02-11 00:26:44 +0300

    solution for 48 problem

--- tasks8.js
+++ tasks8.js
@@ -62,10 +62,8 @@ const astroCollision = (arr) => {
 
 }
 
-// console.log(astroCollision([5, 10, -5]
-  // )); // excepted output would be equal to [10]
-// let a = [10, 2, -5]
-// console.log(a.pop); // excepted output would be equal to [10]
+console.log(astroCollision([5, 10, -5]
+  )); // excepted output would be equal to [10]
 
 
 
@@ -101,3 +99,48 @@ const transpose = (matrix) => {
 };
 
 console.log(transpose([[1,2,3],[4,5,6],[7,8,9]]));
+
+
+// 48. Rotate Image
+// You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+// You have to rotate the image in-place, which means you have to modify the input 2D matrix directly.
+// DO NOT allocate another 2D matrix and do the rotation.
+
+// Constraints:
+//   (n == matrix.length) == matrix[i].length;
+//   1 <= n <= 20
+//   - 1000 <= matrix[i][j] <= 1000;
+
+
+const rotateImg = (matrix) => {
+  const length = matrix.length;
+  for (let i = 0; i < length; i += 1) {
+    for (let j = i; j < length; j += 1) {
+      const refMatrix = matrix[i][j];
+      matrix[i][j] = matrix[j][i];
+      matrix[j][i] = refMatrix;
+    };
+  }
+  for(let i = 0; i < length; i+= 1) {
+    for(let j = 0; j < (length / 2); j += 1) {
+      const refMatrix = matrix[i][j];
+      matrix[i][j] = matrix[i][length - 1 - j];
+      matrix[i][length - 1 - j] = refMatrix;
+    }
+  }
+  return matrix;
+};
+
+console.log(
+  rotateImg([
+    [5, 1, 9, 11],
+    [2, 4, 8, 10],
+    [13, 3, 6, 7],
+    [15, 14, 12, 16],
+  ])
+); // --> expect [
+//   [ 15, 13, 2, 5 ],
+//   [ 14, 3, 4, 1 ],
+//   [ 12, 6, 8, 9 ],
+//   [ 16, 7, 10, 11 ]
+// ]

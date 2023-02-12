124d21088d1257b28832b73e45ff8ca18a5ef40e
katyeheisenberg <chimmi10kate@mail.ru>
2023-02-13 01:01:23 +0300

    448 solution

--- tasks8.js
+++ tasks8.js
@@ -163,3 +163,34 @@ const containsDuplicate = function (nums) {
 };
 
 console.log(containsDuplicate([1, 2, 3, 4])); // --> expect false
+
+// 448. Find All Numbers Disappeared in an Array
+// Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range [1, n] that do not appear in nums.
+// Constraints:
+//     n == nums.length
+//     1 <= n <= 105
+//     1 <= nums[i] <= n
+
+const findDisappearedNumbers = function (nums) {
+  let i = 0;
+  while (i < nums.length) {
+    let position = nums[i] - 1;
+    if (nums[i] !== nums[position]) {
+      const lastPos = nums[i];
+      nums[i] = nums[position];
+      nums[position] = lastPos;
+    } else {
+      i += 1;
+    }
+  }
+  let missing = [];
+  for (let i = 0; i < nums.length; i += 1) {
+    if (nums[i] !== i + 1) {
+      missing = [...missing, i + 1];
+    }
+  }
+
+  return missing;
+};
+
+console.log(findDisappearedNumbers([4, 3, 2, 7, 8, 2, 3, 1])); // --> expect [5, 6]

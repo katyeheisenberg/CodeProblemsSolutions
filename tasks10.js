b1a492d96ed555bda8a1e783c11c96f8e093a0b7
katyeheisenberg <chimmi10kate@mail.ru>
2023-03-18 22:39:20 +0300

    added solution for 55. leetcode problem

--- tasks10.js
+++ tasks10.js
@@ -88,3 +88,35 @@ const notThree = (arr, n) => {
 };
 console.log(notThree([1, 3, 0, 3, 3, 1, 0, 7, 0, 1], 3)); // -> Expect 7
 console.log(notThree([1, 3, 0, 3, 3, 1, 0, 7, 0, 1, 4, 7, 4, 7], 3)); // -> Expect 4
+
+
+// 55. Jump Game
+// You are given an integer array nums. You are initially positioned at the array's first index,
+// and each element in the array represents your maximum jump length at that position.
+// Return true if you can reach the last index, or false otherwise.
+
+// Example 1:
+// Input: nums = [2,3,1,1,4]
+// Output: true
+// Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+
+// Example 2:
+// Input: nums = [3,2,1,0,4]
+// Output: false
+// Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
+
+// Constraints:
+//     1 <= nums.length <= 104
+//     0 <= nums[i] <= 105
+
+const jumpGame = (arr) => {
+  let end = arr.length - 1;
+  for(let i = arr.length -1; i >= 0; i -= 1) {
+    if(i + arr[i] >= end) end = i
+  }
+  return end === 0 ? true : false
+}
+
+console.log(jumpGame([3, 2, 1, 0, 4])); // expect -> fasle
+console.log(jumpGame([3, 2, 1, 5, 4])); // expect -> true
+

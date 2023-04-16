825b1d60bd3ac5a94d71f6bf091b33674f9f3a06
katyeheisenberg <chimmi10kate@mail.ru>
2023-04-16 08:11:32 +0300

    test for 702. LC problem

--- tasks7.js
+++ tasks7.js
@@ -28,7 +28,6 @@ const simplifyPath = (path) => {
 
 exports.simplifyPath = simplifyPath;
 
-
 // 702. Search in a Sorted Array of Unknown Size
 // Given an integer array sorted in ascending order, write a function to search target in nums.
 // If target exists, then return its index, otherwise return -1.
@@ -55,7 +54,7 @@ const findTarget = (arr, target) => {
   }
   while (leftIndex <= rightIndex) {
     let middle = Math.floor((leftIndex + rightIndex) / 2);
-    if (arr[middle] === target) return middle;
+    if (arr[middle] === target) return arr[middle];
     if (arr[leftIndex] <= target && target < arr[middle]) {
       rightIndex = middle - 1;
     } else {
@@ -65,8 +64,7 @@ const findTarget = (arr, target) => {
   return -1;
 };
 
-console.log(findTarget([-1, 0, 3, 5, 9, 12], 9)); // expect 4
-console.log(findTarget([-1, 0, 3, 5, 9, 12], 2)); // expect -1
+exports.findTarget = findTarget;
 
 // 215. Kth Largest Element in an Array
 // Given an integer array nums and an integer k, return the kth largest element in the array.
@@ -75,55 +73,52 @@ console.log(findTarget([-1, 0, 3, 5, 9, 12], 2)); // expect -1
 // O(n * log(n)) solution below
 
 const findKth = (nums, k) => {
-  nums.sort((a, b) => b - a)
-  return nums[k - 1]
-}
-
+  nums.sort((a, b) => b - a);
+  return nums[k - 1];
+};
 
 function part(arr, from, to) {
-  let pivot = from
-  for(let i = from; i < to; i += 1) {
-    if(arr[i] > arr[to]) {
-      [arr[i], arr[pivot]] = [arr[pivot], arr[i]]
-      pivot += 1
+  let pivot = from;
+  for (let i = from; i < to; i += 1) {
+    if (arr[i] > arr[to]) {
+      [arr[i], arr[pivot]] = [arr[pivot], arr[i]];
+      pivot += 1;
     }
   }
-  [arr[pivot], arr[to]] = [arr[to], arr[pivot]]
-  return pivot
+  [arr[pivot], arr[to]] = [arr[to], arr[pivot]];
+  return pivot;
 }
 
-const findKth2 = (nums, k) => {     // O(N)
-  const idx = part(nums, 0, nums.length - 1)
-  if(idx === k - 1) return nums[idx]
-  if(idx < k) return findKth2(nums.slice(idx + 1), k - idx - 1)
-  return findKth2(nums.slice(0, idx), k)
-}
+const findKth2 = (nums, k) => {
+  // O(N)
+  const idx = part(nums, 0, nums.length - 1);
+  if (idx === k - 1) return nums[idx];
+  if (idx < k) return findKth2(nums.slice(idx + 1), k - idx - 1);
+  return findKth2(nums.slice(0, idx), k);
+};
 console.log(findKth2([3, 2, 1, 5, 6, 4], 2)); // expect 5
-
-
 
 // Task from university
 
 const gi = (w1, w2) => {
-  let howManyLetters2ndWord = 0
+  let howManyLetters2ndWord = 0;
   let indexOfUselessLetter1stword = w1.length - 2 - howManyLetters2ndWord;
-  let firstpart = w1.slice(0,indexOfUselessLetter1stword)
-  for(let i = 0; i < w1.length; i += 1) {
-    if(w1[i] === w2[howManyLetters2ndWord]) {
-      howManyLetters2ndWord += 1
+  let firstpart = w1.slice(0, indexOfUselessLetter1stword);
+  for (let i = 0; i < w1.length; i += 1) {
+    if (w1[i] === w2[howManyLetters2ndWord]) {
+      howManyLetters2ndWord += 1;
     }
-    if(howManyLetters2ndWord === w1.length) {
-      return w2
+    if (howManyLetters2ndWord === w1.length) {
+      return w2;
     }
-    if(howManyLetters2ndWord === 0) {
-      return `${w1}${w2}`
+    if (howManyLetters2ndWord === 0) {
+      return `${w1}${w2}`;
     }
     // console.log(howManyLetters2ndWord)
-
   }
   return `${firstpart}${w2}`;
-}
-console.log(gi('шлакоблок','окунь'))
-console.log(gi('папа','папаха'))
-console.log(gi('гора','радуга'))
-console.log(gi('груша','спичка'))
+};
+console.log(gi("шлакоблок", "окунь"));
+console.log(gi("папа", "папаха"));
+console.log(gi("гора", "радуга"));
+console.log(gi("груша", "спичка"));
--- tests/tasks7.test.js
+++ tests/tasks7.test.js
@@ -1,5 +1,6 @@
 const {
-  simplifyPath
+  simplifyPath,
+  findTarget
 } = require("../tasks7");
 
 describe("tests of simplify function", function() {
@@ -9,4 +10,13 @@ describe("tests of simplify function", function() {
   test("convert /home//foo/ into /home/foo", function () {
     expect(simplifyPath("/home//foo/")).toEqual("/home/foo");
   });
-})
+});
+
+describe("search a specific number in a sorted array of unknown size", function () {
+  test("find 9", function () {
+    expect(findTarget([-1, 0, 3, 5, 9, 12], 9)).toBe(9);
+  });
+  test("find 2 which is not in the array", function () {
+    expect(findTarget([-1, 0, 3, 5, 9, 12], 2)).toBe(-1);
+  });
+});
